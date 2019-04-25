1df755e1ff64cea8e7baa8a4d4801809e011e1da
郭玉光 <guoyuguang@higinet.com.cn>
2019-04-25 10:21:36 +0800

    test git

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    hello world
+    Git is a distributed version control system.
+    Git is free software distributed under the GPL.
+    Git has a mutable index called stage.
   </div>
 </template>
 
